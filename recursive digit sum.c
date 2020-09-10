4c060530ba8f46386bdde1d7ff9918421deff0db
Sahil Goyal <68017381+DeveloperSahil2001@users.noreply.github.com>
2020-09-10 11:03:13 +0530

    Create recursive digit sum.c

---
+++ recursive digit sum.c
@@ -0,0 +1,121 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+int getSum(long long int val)
+{
+ if(val<10)
+ {
+  return val;
+ }
+ long long int sum=0;
+ do{
+  sum+=val%10;
+  val/=10;
+ }
+ while(val>0);
+ return getSum(sum);
+}
+
+// Complete the superDigit function below.
+int superDigit(char* n, int k) {
+ long long int sum=0,i;
+ for(i=0;i<strlen(n);i++)
+ {
+  sum+=(n[i]-'0');
+ }
+ return getSum(sum*k);
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** nk = split_string(readline());
+
+    char* n = nk[0];
+
+    char* k_endptr;
+    char* k_str = nk[1];
+    int k = strtol(k_str, &k_endptr, 10);
+
+    if (k_endptr == k_str || *k_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    int result = superDigit(n, k);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!line) {
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+    } else {
+        data = realloc(data, data_length + 1);
+
+        data[data_length] = '\0';
+    }
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
